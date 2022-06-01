54fccf63c36d8a04f2d669f0818196d4b913c401
aviligonda <aviligondasreenivasulu6086@gmail.com>
2022-06-01 15:59:28 +0530

    Daily wage

---
+++ DailyWage.java
@@ -0,0 +1,10 @@
+public class DailyWage {
+    public static void main(String[] args) {
+        int Wage_per_hr=20;
+        int Full_day_hr=8;
+        int daily_wage;
+            System.out.println("Employee is present in Full day");
+            daily_wage=Wage_per_hr*Full_day_hr;
+            System.out.println("Employee daily wage is = "+daily_wage);
+    }
+}
